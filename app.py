c187b925495907ede8947f0bd71771d4f7ed77b0
cisco836 <tfurqan836@gmail.com>
2024-07-28 17:14:07 +0500

    fixed context and duplicates

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# app.py
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
@@ -8,25 +9,32 @@ from services.utils import generate_unique_id
 from services.embeddings import extract_embeddings, compare_embeddings
 from services.video_processor import VideoProcessor
 
-app = Flask(__name__)
-CORS(app, origins="http://192.168.1.9:3000")
 
-# Initialize MongoDB client once at startup
-mongo_client = MongoClient(Config.MONGO_URI)
-app.config['mongo_client'] = mongo_client
-app.config['generate_unique_id'] = generate_unique_id
-app.config['extract_embeddings'] = extract_embeddings
-app.config['compare_embeddings'] = compare_embeddings
-app.config['VideoProcessor'] = VideoProcessor
+def create_app():
+    app = Flask(__name__)
+    CORS(app, origins="http://192.168.1.9:3000")
 
-#Initialize Firebase
-initialize_firebase()
+    # Initialize MongoDB client once at startup
+    with app.app_context():
+        mongo_client = MongoClient(Config.MONGO_URI)
+        app.config['mongo_client'] = mongo_client
+        app.config['generate_unique_id'] = generate_unique_id
+        app.config['extract_embeddings'] = extract_embeddings
+        app.config['compare_embeddings'] = compare_embeddings
+        app.config['VideoProcessor'] = VideoProcessor
+        # app.config['insertion_bool'] = False
 
-# Register blueprints
-app.register_blueprint(default.bp)
-app.register_blueprint(upload.bp)
-app.register_blueprint(stream.bp)
-app.register_blueprint(metadata.bp)
+        # Initialize Firebase
+        initialize_firebase()
+
+        # Register blueprints
+        app.register_blueprint(default.bp)
+        app.register_blueprint(upload.bp)
+        app.register_blueprint(stream.bp)
+        app.register_blueprint(metadata.bp)
+
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(host="0.0.0.0", port=5000, debug=True, threaded=True)
